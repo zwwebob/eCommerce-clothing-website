5b0784497746e72b886dfc2d0ef30fd1faf02752
binZuy <duynguyenminh0206@gmail.com>
2024-11-10 02:32:21 +0700

    Design view for cart

--- front-end/src/Components/DescriptionBox/DescriptionBox.jsx
+++ front-end/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import './DescriptionBox.CSS'
+import React from "react";
+import "./DescriptionBox.css";
 
 const DescriptionBox = () => {
   return (
     <div className="descriptionbox">
-        
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Review (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          An e-commerce website is an online platform that facilitate buying and
+          selling products of services over the internet serves as a virtual
+          marketplace where businesses and individual showcase their products,
+          interact with customers, and conduct transactions without the need for
+          a physical presence. E-commerce websites have gained immense
+          popularity due to their convenient accessibility, and the global reach
+          they offer.
+        </p>
+        <p>
+          E-commerce webiste typically display products or services along with
+          detailed descriptions, images, prices, and any available variations
+          (e.g, sizes, colors). Each product usually has its own dedicated pages
+          with relevant information.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DescriptionBox
+export default DescriptionBox;
